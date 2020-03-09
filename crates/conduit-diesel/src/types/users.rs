8957ef1a62a7e3b3d0264d9e01aee2ffa290f5c0
Jet Li <jing.i.qin@icloud.com>
2020-03-09 19:09:14 +0800

    Add User struct

---
+++ crates/conduit-diesel/src/types/users.rs
@@ -0,0 +1,15 @@
+use chrono::prelude::*;
+use diesel::Queryable;
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct User {
+    pub id: u64,
+    pub username: String,
+    pub email: String,
+    pub password: String,
+    pub bio: Option<String>,
+    pub image: Option<String>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
