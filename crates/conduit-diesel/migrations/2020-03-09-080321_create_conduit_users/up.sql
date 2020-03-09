89ae5db34b03782dbe44570e689e6a6257af324b
Jet Li <jing.i.qin@icloud.com>
2020-03-09 18:24:31 +0800

    Format migration for users table

--- crates/conduit-diesel/migrations/2020-03-09-080321_create_conduit_users/up.sql
+++ crates/conduit-diesel/migrations/2020-03-09-080321_create_conduit_users/up.sql
@@ -1,12 +1,13 @@
--- Your SQL goes here
-CREATE TABLE conduit_users (
-    id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    bio VARCHAR(2048),
-    image VARCHAR(2048),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE KEY (username, email)
-)
+CREATE TABLE `conduit_users` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `bio` varchar(2048) DEFAULT NULL,
+    `image` varchar(2048) DEFAULT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `username` (`username`),
+    UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
